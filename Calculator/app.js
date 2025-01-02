cf5f5c47511d7ed4fb432332e1d5182498c54c54
Akash <86909011+Akash0981@users.noreply.github.com>
2025-01-02 21:53:47 +0530

    Update app.js

--- Calculator/app.js
+++ Calculator/app.js
@@ -1,1 +1,23 @@
+const buttons = document.getElementsByTagName("button");
 
+let length = buttons.length;
+let result = document.getElementById("result");
+
+
+for(i = 0; i< length; i++){
+    buttons[i].onclick = indicator;
+
+}
+
+function indicator(){
+    let value = this.innerHTML;
+    if(value == "="){
+        try{
+            result.value = eval(result.value);
+        }catch(e){
+            result.value = "0";
+        }
+        return;
+    }
+    result.value += value;
+}
