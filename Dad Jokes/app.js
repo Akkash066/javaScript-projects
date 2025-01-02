c94424ca89cf238932962d5b762fca693c725a8c
Akash <86909011+Akash0981@users.noreply.github.com>
2025-01-02 21:21:44 +0530

    Update app.js

--- Dad Jokes/app.js
+++ Dad Jokes/app.js
@@ -1,22 +1,15 @@
 const btn = document.getElementById("btn");
-
 const apikey = "api key";
-
 const options = {
     method: "GET", 
     headers: {
         "X-Api-key": apikey,
     },
-
 };
-
 const apiURL = "api url";
 async function getJoke(){
     const response = await fetch(apiURL, options);
     const data = response.json();
-
     console.log(data);
 }
-
-
 btn.addEventListener("click", getJoke)
