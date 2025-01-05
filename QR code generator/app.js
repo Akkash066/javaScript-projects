732e12b31e0a4565561c65c4e094799f7921a66d
Akash <86909011+Akash0981@users.noreply.github.com>
2025-01-05 21:55:11 +0530

    Update app.js

--- QR code generator/app.js
+++ QR code generator/app.js
@@ -1,2 +1,18 @@
 const data = document.querySelector("#text");
 const qrCode = document.querySelector("#qrCode");
+
+
+const data = document.querySelector("#text");
+const qrCode = document.querySelector("#qrCode");
+const Generator = document.querySelector("#Generator");
+
+const url = "https://api.qrserver.com/v1/create-qr-code/"
+
+Generator.addEventListener("click", () => {
+    const size = `350x350`;
+    qrCode.src = `${url}?/size=${size}&data=${data.value}`;
+    console.log("clicked");
+    if(data.value == ""){
+        qrCode.src = "/def.png";
+    }
+})
